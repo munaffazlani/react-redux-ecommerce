882e214f0dd5def84e6e5fe532f8d5019a6217bb
munaffazlani <munaffazlani@gmail.com>
2020-02-21 08:31:18 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider, useSelector } from "react-redux";
+import { createStore } from "redux";
+import allReducers from "./reducers/all.js";
+import "./index.css";
+import "./filtersboxstyle.css";
+import "./productCard.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const loadstate = () => {
+//   try {
+//     const state = localStorage.getItem("state");
+//     if (state == null) return undefined;
+//     return JSON.parse(state);
+//   } catch (e) {
+//     console.log(e);
+//     return undefined;
+//   }
+// };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const saveToLocalStorage = state => {
+  try {
+    localStorage.setItem("state", JSON.stringify(state));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const loadState = () => {
+  try {
+    const localState = localStorage.getItem("state");
+    if (localState === null) return undefined;
+    return JSON.parse(localState);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+};
+
+const localStorageState = loadState();
+const store = createStore(allReducers, localStorageState);
+
+//Subscribe my store to local storage so Everytime state will change, local storage will updates
+store.subscribe(() => saveToLocalStorage(store.getState()));
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />{" "}
+  </Provider>,
+  rootElement
+);
